83ef95517e686f050eb7c11154b218af57c5e0f8
valeriec7 <valeriecalle247@gmail.com>
2024-10-07 11:45:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
-
 const http = require('http');
+const url = require('url')
 
 // here the server created
 const server = http.createServer((req, res) => {
-    // Set the response headers
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+   // Set the response headers
+   const parseUrl = url.parse(req.url, true);
+   const queryParams = parseUrl.query;
 
-    // Write the response content
-    res.write('<h1>Hi, please let me know what ill return </h1>');
+
+   res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    if (Object.keys(queryParams).length > 0){
+       res.write('<h1> I got what you send bro : </h1>')
+       res.write('<ul>')
+       for (const param in queryParams){
+           res.write(`<l1>${param} : ${queryParams[param]}</l1>`)
+       }
+       res.write('</ul>');
+      
+      
+    }else{
+       res.write('<h1>Hi just normal like before</h1>');
+    }
+
     res.end();
+
 });
 
 // Specify the port to listen on
@@ -16,5 +32,5 @@ const port = 3000;
 
 // server will begin listening after this command
 server.listen(port, () => {
-    console.log(`Node.js HTTP server is running on port ${port}`);
+   console.log(`Node.js HTTP server is running on port ${port}`);
 });
